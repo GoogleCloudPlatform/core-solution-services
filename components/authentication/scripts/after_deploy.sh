5ed58e7c129432a613fdd36621a28a7e3dd8fbdd
Jon Chen <jonchen@google.com>
2023-08-14 22:52:47 -0400

    update auth after_deploy

--- components/authentication/scripts/after_deploy.sh
+++ components/authentication/scripts/after_deploy.sh
@@ -13,4 +13,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-PYTHONPATH=components/common/src/ python components/authentication/scripts/user_tool.py create_user
+PYTHONPATH=components/common/src/ python user_tool.py create_user
