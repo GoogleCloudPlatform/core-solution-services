0cb5e8119ef2f8e944d0f083c2c3f0f4500c8b5f
Lukman Ramsey <lramsey@google.com>
2023-10-07 11:40:43 -0400

    add method, class comments to web_datasource

--- components/llm_service/src/services/web_datasource.py
+++ components/llm_service/src/services/web_datasource.py
@@ -21,17 +21,33 @@ from langchain.document_loaders.base import BaseLoader
 import os
 
 class WebDataSourceSpider(scrapy.Spider):
+  """Scrapy spider to crawl and download webpages."""
+  
   name = "web_data_source_spider"
   
   def __init__(self, start_urls=None, filepath=None, *args, **kwargs):
+    """
+    Initialize the spider.
+    
+    Args:
+      start_urls: List of URLs to start crawling from.
+      filepath: Directory path to save downloaded webpages.
+    """
     super(WebDataSourceSpider, self).__init__(*args, **kwargs)
     self.start_urls = start_urls or []
     self.filepath = filepath
 
   def parse(self, response):
+    """
+    Parse the downloaded webpage and save its content.
+    
+    Args:
+      response: The response object containing webpage data.
+    """
     # Save the content to the specified filepath
     if self.filepath:
-      with open(os.path.join(self.filepath, response.url.split("/")[-1] + ".html"), "w") as f:
+      with open(os.path.join(self.filepath, 
+                             response.url.split("/")[-1] + ".html"), "w") as f:
         f.write(response.text)
     
     # Extract data and save or process it
@@ -41,13 +57,27 @@ class WebDataSourceSpider(scrapy.Spider):
     }
 
 class WebDataSource(BaseLoader):
+  """Document loader that uses Scrapy to download webpages."""
 
   def __init__(self, urls, filepath=None):
+    """
+    Initialize the WebDataSource.
+    
+    Args:
+      urls: List of URLs to download.
+      filepath: Directory path to save downloaded webpages.
+    """
     self.urls = urls
     self.filepath = filepath
     self.documents = []
 
   def load(self) -> List[Document]:
+    """
+    Load the webpages and return them as a list of Document objects.
+    
+    Returns:
+      List of Document objects.
+    """
     # Define the Scrapy settings
     settings = {
       "USER_AGENT": ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
@@ -66,6 +96,11 @@ class WebDataSource(BaseLoader):
       yield Document(url=data["url"], content=data["content"])
 
   def lazy_load(self) -> Iterator[Document]:
-    # This method will be a generator that yields Document objects
+    """
+    Lazily load the webpages and yield them as Document objects.
+    
+    Returns:
+      Iterator of Document objects.
+    """
     for doc in self.load():
       yield doc
