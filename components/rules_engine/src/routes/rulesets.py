dd05e006621b4b7b40c818ed67106d201c94f083
Jon Chen <jonchen@google.com>
2023-10-14 04:27:44 +0800

    added RulesetFieldsSchema

--- components/rules_engine/src/routes/rulesets.py
+++ components/rules_engine/src/routes/rulesets.py
@@ -14,7 +14,7 @@
 
 from fastapi import APIRouter, HTTPException
 from models.rule import Rule
-from schemas.rule import RuleSchema
+from schemas.ruleset import RulesetFieldsSchema
 import datetime
 
 # disabling for linting to pass
@@ -24,6 +24,7 @@ router = APIRouter(prefix="/ruleset", tags=["ruleset"])
 
 SUCCESS_RESPONSE = {"status": "Success"}
 
+# RuleSet for eligibility.
 MOCK_RULESET = {
   "id": "ruleset-1",
   "labels": ["eligibility"],
@@ -32,57 +33,57 @@ MOCK_RULESET = {
     {
       "description": "Income must be below the Federal Poverty Level (FPL). The FPL for 2023 is $14,580 for an individual and $30,000 for a family of four",
       "fields": {
-        "Individual Income": int,
-        "Family Income": int,
+        "Individual Income": "int",
+        "Family Income": "int",
       },
       "sql_query": "",
     },
     {
       "description": "Citizenship or immigration status: Must be a U.S. citizen, a qualified non-citizen, or a qualified immigrant",
       "fields": {
-        "Citizenship or immigration status": str
+        "Citizenship or immigration status": "str"
       },
       "sql_query": "",
     },
     {
       "description": "Medicaid beneficiaries must be residents of the state in which they are applying",
       "fields": {
-        "Resident state": str
+        "Resident state": "str"
       },
       "sql_query": "",
     },
     {
       "description": "Medicaid is available to all children under age 19, regardless of their family's income; Medicaid is available to people who are 65 years old or older and meet certain income and asset requirements.",
       "fields": {
-        "Age": int
+        "Age": "int"
       },
       "sql_query": "",
     },
     {
       "description": "Disability status: People with disabilities may be eligible for Medicaid if they meet certain criteria. The criteria vary from state to state, but they typically involve having a physical or mental impairment that limits major life activities.",
       "fields": {
-        "Disability status": str
+        "Disability status": "str"
       },
       "sql_query": "",
     },
     {
       "description": "Pregnancy",
       "fields": {
-        "Pregnancy status": str
+        "Pregnancy status": "str"
       },
       "sql_query": "",
     },
     {
       "description": "Nursing home residency: People who live in nursing homes may be eligible for Medicaid if they meet certain income and asset requirements",
       "fields": {
-        "Nursing home residency": str
+        "Nursing home residency": "str"
       },
       "sql_query": "",
     },
     {
       "description": "HCBS eligibility: People who receive HCBS may be eligible for Medicaid if they meet certain income and asset requirements. HCBS are waiver services that are provided outside of a nursing home or other institution.",
       "fields": {
-        "HCBS status": str
+        "HCBS status": "str"
       },
       "sql_query": "",
     },
@@ -109,7 +110,7 @@ async def get(id: str):
   return MOCK_RULESET
 
 
-@router.get("/{id}/fields")
+@router.get("/{id}/fields", response_model=RulesetFieldsSchema)
 async def get_fields(id: str):
   """Get all fields in a RuleSet
 
@@ -125,6 +126,7 @@ async def get_fields(id: str):
   """
 
   fields = {}
+
   for rule in MOCK_RULESET["rules"]:
     fields.update(rule.get("fields", {}))
 
---
+++ components/rules_engine/src/schemas/ruleset.py
@@ -0,0 +1,39 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Pydantic Model for Rule API's
+
+from typing import Optional
+from pydantic import BaseModel
+from datetime import datetime
+
+from typing import List, Optional
+from pydantic import BaseModel
+
+class RulesetFieldsSchema(BaseModel):
+  """Ruleset Fields Pydantic Model"""
+
+  # This is the reference API spec for Rule data model.
+  fields: dict = {}
+
+  class Config():
+    orm_mode = True
+    schema_extra = {
+      "example": {
+        "fields": {
+          "field-1": str,
+          "field-2": int,
+        }
+      }
+    }
