1f042c4a748cdb367fe9af36bd088de64e61f419
Jon Chen <jonchen@google.com>
2023-10-14 04:09:47 +0800

    add get_fields to rulesets API

--- components/rules_engine/src/routes/rulesets.py
+++ components/rules_engine/src/routes/rulesets.py
@@ -24,6 +24,72 @@ router = APIRouter(prefix="/ruleset", tags=["ruleset"])
 
 SUCCESS_RESPONSE = {"status": "Success"}
 
+MOCK_RULESET = {
+  "id": "ruleset-1",
+  "labels": ["eligibility"],
+  "name": "Financial eligibility",
+  "rules": [
+    {
+      "description": "Income must be below the Federal Poverty Level (FPL). The FPL for 2023 is $14,580 for an individual and $30,000 for a family of four",
+      "fields": {
+        "Individual Income": int,
+        "Family Income": int,
+      },
+      "sql_query": "",
+    },
+    {
+      "description": "Citizenship or immigration status: Must be a U.S. citizen, a qualified non-citizen, or a qualified immigrant",
+      "fields": {
+        "Citizenship or immigration status": str
+      },
+      "sql_query": "",
+    },
+    {
+      "description": "Medicaid beneficiaries must be residents of the state in which they are applying",
+      "fields": {
+        "Resident state": str
+      },
+      "sql_query": "",
+    },
+    {
+      "description": "Medicaid is available to all children under age 19, regardless of their family's income; Medicaid is available to people who are 65 years old or older and meet certain income and asset requirements.",
+      "fields": {
+        "Age": int
+      },
+      "sql_query": "",
+    },
+    {
+      "description": "Disability status: People with disabilities may be eligible for Medicaid if they meet certain criteria. The criteria vary from state to state, but they typically involve having a physical or mental impairment that limits major life activities.",
+      "fields": {
+        "Disability status": str
+      },
+      "sql_query": "",
+    },
+    {
+      "description": "Pregnancy",
+      "fields": {
+        "Pregnancy status": str
+      },
+      "sql_query": "",
+    },
+    {
+      "description": "Nursing home residency: People who live in nursing homes may be eligible for Medicaid if they meet certain income and asset requirements",
+      "fields": {
+        "Nursing home residency": str
+      },
+      "sql_query": "",
+    },
+    {
+      "description": "HCBS eligibility: People who receive HCBS may be eligible for Medicaid if they meet certain income and asset requirements. HCBS are waiver services that are provided outside of a nursing home or other institution.",
+      "fields": {
+        "HCBS status": str
+      },
+      "sql_query": "",
+    },
+  ],
+}
+
+
 
 @router.get("/{id}")
 async def get(id: str):
@@ -40,69 +106,16 @@ async def get(id: str):
     ruleset: a ruleset object that contains a list of rules.
   """
 
-  ruleset = {
-    "id": "ruleset-1",
-    "labels": ["eligibility"],
-    "name": "Financial eligibility",
-    "rules": [
-      {
-        "description": "Income must be below the Federal Poverty Level (FPL). The FPL for 2023 is $14,580 for an individual and $30,000 for a family of four",
-        "fields": {
-          "Individual Income": int,
-          "Family Income": int,
-        },
-        "sql_query": "",
-      },
-      {
-        "description": "Citizenship or immigration status: Must be a U.S. citizen, a qualified non-citizen, or a qualified immigrant",
-        "fields": {
-          "Citizenship or immigration status": str
-        },
-        "sql_query": "",
-      },
-      {
-        "description": "Medicaid beneficiaries must be residents of the state in which they are applying",
-        "fields": {
-          "Resident state": str
-        },
-        "sql_query": "",
-      },
-      {
-        "description": "Medicaid is available to all children under age 19, regardless of their family's income; Medicaid is available to people who are 65 years old or older and meet certain income and asset requirements.",
-        "fields": {
-          "Age": int
-        },
-        "sql_query": "",
-      },
-      {
-        "description": "Disability status: People with disabilities may be eligible for Medicaid if they meet certain criteria. The criteria vary from state to state, but they typically involve having a physical or mental impairment that limits major life activities.",
-        "fields": {
-          "Disability status": str
-        },
-        "sql_query": "",
-      },
-      {
-        "description": "Pregnancy",
-        "fields": {
-          "Pregnancy status": str
-        },
-        "sql_query": "",
-      },
-      {
-        "description": "Nursing home residency: People who live in nursing homes may be eligible for Medicaid if they meet certain income and asset requirements",
-        "fields": {
-          "Nursing home residency": str
-        },
-        "sql_query": "",
-      },
-      {
-        "description": "HCBS eligibility: People who receive HCBS may be eligible for Medicaid if they meet certain income and asset requirements. HCBS are waiver services that are provided outside of a nursing home or other institution.",
-        "fields": {
-          "HCBS status": str
-        },
-        "sql_query": "",
-      },
-    ],
-  }
+  return MOCK_RULESET
 
-  return ruleset
+
+@router.get("/{id}/fields")
+async def get_fields(id: str):
+
+  fields = {}
+  for rule in MOCK_RULESET["rules"]:
+    fields.update(rule.get("fields", {}))
+
+  return {
+    "fields": fields
+  }
