e0d57203064573396ee0a9ac96583712ead6e8c5
Lav Jain <lavjain@google.com>
2023-11-08 19:36:07 -0500

    User tool lint/refactor (#164)

--- components/authentication/scripts/user_tool.py
+++ components/authentication/scripts/user_tool.py
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-
+# pylint: disable = broad-exception-raised, broad-exception-caught
 import os
 import subprocess
 import requests
@@ -26,8 +26,11 @@ from common.models import User
 from google.cloud import secretmanager
 
 PROJECT_ID = os.environ.get("PROJECT_ID")
+assert PROJECT_ID, "PROJECT_ID is not set."
 AUTH_API_PATH = "authentication/api/v1"
-DEFAULT_BOT_USERNAME="mira-bot@google.com"
+DEFAULT_BOT_USERNAME = "mira-bot@google.com"
+LLM_BACKEND_ROBOT_USERNAME = "llm-backend-robot-username"
+LLM_BACKEND_ROBOT_PASSWORD = "llm-backend-robot-password"
 
 USER_DATA = {
     "first_name": "test",
@@ -40,14 +43,12 @@ USER_DATA = {
     "access_api_docs": True,
     "gaia_id": "fake-gaia-id",
 }
-warnings.filterwarnings('ignore', message='Unverified HTTPS request')
+warnings.filterwarnings("ignore", message="Unverified HTTPS request")
 sm_secrets = secretmanager.SecretManagerServiceClient()
-
 
 def execute_command(command):
   output = subprocess.check_output(command, shell=True, text=True)
   return output.strip()
-
 
 def get_input(prompt):
   input_value = None
@@ -55,31 +56,38 @@ def get_input(prompt):
     input_value = input(prompt)
   return input_value
 
-def add_secret(secret_name, secret_value):
-  if PROJECT_ID is None:
-    assert PROJECT_ID, "PROJECT_ID is not set."  
+def add_secret(secret_name, secret_value) -> str:
   parent = f"projects/{PROJECT_ID}/secrets/{secret_name}"
-  payload  = secret_value.encode('UTF-8')
-  response = sm_secrets.add_secret_version(parent=parent, 
-                              payload={
-                                  'data': payload
-                              })
+  payload = secret_value.encode("utf-8")
+  version = sm_secrets.add_secret_version(
+    request={"parent": parent, "payload": {"data": payload}}
+  )
+  return str(version)
 
-def get_secret_value(secret_name):
+def get_secret_value(secret_name) -> str:
+  secret_value = sm_secrets.access_secret_version(
+      request={"name": f"projects/{PROJECT_ID}" +
+                       f"/secrets/{secret_name}/versions/latest"}
+  ).payload.data.decode("utf-8")
+  return secret_value.strip()
+
+def create_bot_account() -> (str, str):
+  # Get llm-backend robot username
   try:
-    secret_value = sm_secrets.access_secret_version(
-        request={
-            "name": f"projects/{PROJECT_ID}" +
-                f"/secrets/{secret_name}/versions/latest"
-        }).payload.data.decode("utf-8")
-    return secret_value.strip()
-  except Exception as e:
-    return None
+    username = get_secret_value(LLM_BACKEND_ROBOT_USERNAME)
+  except Exception:
+    username = DEFAULT_BOT_USERNAME
+  add_secret(LLM_BACKEND_ROBOT_USERNAME, username)
+  # Get llm-backend robot password
+  try:
+    password = get_secret_value(LLM_BACKEND_ROBOT_PASSWORD)
+  except Exception:
+    password = secrets.token_urlsafe(16)
+  add_secret(LLM_BACKEND_ROBOT_PASSWORD, password)
+  return username, password
 
 def create_user(user_email, user_password, base_url=None) -> None:
-  """
-  Function to do firebase login
-  """
+  # Create user in firebase
   input_user = {**USER_DATA, "email": user_email}
   user = User.from_dict(input_user)
   user.user_id = ""
@@ -90,7 +98,7 @@ def create_user(user_email, user_password, base_url=None) -> None:
     "password": user_password
   }
   url = f"{base_url}/{AUTH_API_PATH}/sign-up/credentials"
-  sign_up_req = requests.post(url, json=req_body, verify=False)
+  sign_up_req = requests.post(url, json=req_body, verify=False, timeout=10)
   sign_up_res = sign_up_req.json()
 
   # If returns 200, the user was created successfully. Print the token then.
@@ -109,14 +117,13 @@ def create_user(user_email, user_password, base_url=None) -> None:
     print(f"Sign up error. Status: {sign_up_req.status_code}")
     print(sign_up_res)
 
-
 def login_user(user_email, user_password, base_url=None) -> None:
   req_body = {
     "email": user_email,
     "password": user_password
   }
   url = f"{base_url}/{AUTH_API_PATH}/sign-in/credentials"
-  sign_in_req = requests.post(url, json=req_body, verify=False)
+  sign_in_req = requests.post(url, json=req_body, verify=False, timeout=10)
 
   sign_in_res = sign_in_req.json()
   if sign_in_res is None or sign_in_res["data"] is None:
@@ -128,10 +135,10 @@ def login_user(user_email, user_password, base_url=None) -> None:
   print(id_token)
   print()
 
-
 def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument("action", type=str, help="Main action: [create_user|get_token|create_bot_account]")
+  help_str = "Main action: [create_user|get_token|create_bot_account]"
+  parser.add_argument("action", type=str, help=help_str)
   parser.add_argument("--base-url", type=str, help="API base URL")
   args = parser.parse_args()
 
@@ -151,7 +158,8 @@ def main():
     user = User.find_by_email(user_email)
     if user is None:
       # Create new user
-      user_password = getpass.getpass(prompt="Password (At least 6 alphanumeric): ")
+      user_password = getpass.getpass(
+        prompt="Password (At least 6 alphanumeric): ")
       confirm_password = getpass.getpass(prompt="Confirm password: ")
       assert user_password == confirm_password, "Passwords don't match."
       create_user(user_email, user_password, base_url=base_url)
@@ -160,21 +168,15 @@ def main():
       login_user(user_email, user_password, base_url=base_url)
 
   elif args.action == "get_token":
+    user_email = input(f"User email ({user_email}): ") or user_email
     user_password = getpass.getpass(prompt="Password: ")
     print()
     login_user(user_email, user_password, base_url=base_url)
 
   elif args.action == "create_bot_account":
-    llm_backend_robot_username_value = get_secret_value("llm-backend-robot-username")
-    if llm_backend_robot_username_value is None:
-      add_secret("llm-backend-robot-username", DEFAULT_BOT_USERNAME)
-
-    llm_backend_robot_password_value = get_secret_value("llm-backend-robot-password")
-    if llm_backend_robot_password_value is None:
-      llm_backend_robot_password_value = secrets.token_urlsafe(16)
-      add_secret("llm-backend-robot-password", llm_backend_robot_password_values)
-
-    create_user(llm_backend_robot_username_value, llm_backend_robot_password_value, base_url=base_url)
+    username, password = create_bot_account()
+    # TODO: Create user only if not already present
+    create_user(username, password, base_url=base_url)
 
   else:
     print(f"Action {args.action} not supported. Available actions:")
