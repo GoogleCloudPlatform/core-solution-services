e649f4fca5103f4771f60e00cb9ed0f36278444a
Lukman Ramsey <lramsey@google.com>
2023-11-09 01:23:22 -0500

    made data source a class

--- components/llm_service/src/services/query/data_source.py
+++ components/llm_service/src/services/query/data_source.py
@@ -14,8 +14,12 @@
 """
 Query Data Sources
 """
+import os
+from typing import List, Tuple
 from pathlib import Path
+from common.models import QueryDocument
 from common.utils.logging_handler import Logger
+from google.cloud import storage
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.document_loaders import CSVLoader
 from pypdf import PdfReader
@@ -24,108 +28,116 @@ from utils.errors import NoDocumentsIndexedException
 # text chunk size for embedding data
 CHUNK_SIZE = 1000
 
-def download_documents(doc_url: str, temp_dir: Path) -> List[str]:
+class DataSource:
 
-  # download files to local directory
-  doc_filepaths = download_files_to_local(storage_client, temp_dir, doc_url)
+  def __init__(self, storage_client):
+    self.storage_client = storage_client
+    self.docs_not_processed = []
 
-  if len(doc_filepaths) == 0:
-    raise NoDocumentsIndexedException(
-        f"No documents can be indexed at url {doc_url}")
-  return doc_filepaths
+  @property
+  def docs_not_processed(self) -> List[str]:
+    return self.docs_not_processed
 
+  def download_documents(self, doc_url: str,
+                         temp_dir: Path) -> List[str]:
 
-def chunk_document(doc_name: str, doc_url: str, doc_filepath: str) -> \
-                    Tuple[List[QueryDocument], List[str]]:
-  """
-  Process docs at url and upload embeddings to GCS for indexing.
-  Returns:
-     Tuple of list of QueryDocument objects for docs processed,
-        list of doc urls of docs not processed
-  """
+    # download files to local directory
+    doc_filepaths = download_files_to_local(temp_dir, doc_url)
 
-  # use langchain text splitter
-  text_splitter = CharacterTextSplitter(chunk_size=CHUNK_SIZE,
-                                        chunk_overlap=0)
-
-  # add embeddings for each doc to index data stored in bucket
-  docs_processed = []
-  docs_not_processed = []
-
-  Logger.info(f"generating index data for {doc_name}")
-
-  # read doc data and split into text chunks
-  # skip any file that can't be read or generates an error
-  try:
-    doc_text_list = read_doc(doc_name, doc_filepath)
-    if doc_text_list is None:
-      Logger.error(f"no content read from {doc_name}")
-      docs_not_processed.append(doc_url)
-      continue
-  except Exception as e:
-    Logger.error(f"error reading doc {doc_name}: {e}")
-    docs_not_processed.append(doc_url)
-    continue
-
-  # split text into chunks
-  text_chunks = []
-  for text in doc_text_list:
-    text_chunks.extend(text_splitter.split_text(text))
-
-  if all(element == "" for element in text_chunks):
-    Logger.warning(f"All extracted pages from {doc_name} are empty.")
-    docs_not_processed.append(doc_url)
-    continue
-
-  return text_chunks
+    if len(doc_filepaths) == 0:
+      raise NoDocumentsIndexedException(
+          f"No documents can be indexed at url {doc_url}")
+    return doc_filepaths
 
 
-def download_files_to_local(storage_client, local_dir,
-                             doc_url: str) -> List[Tuple[str, str, str]]:
-  """ Download files from GCS to a local tmp directory """
-  docs = []
-  bucket_name = doc_url.split("gs://")[1].split("/")[0]
-  Logger.info(f"downloading {doc_url} from bucket {bucket_name}")
-  for blob in storage_client.list_blobs(bucket_name):
-    # Download the file to the tmp folder flattening all directories
-    file_name = Path(blob.name).name
-    file_path = os.path.join(local_dir, file_name)
-    blob.download_to_filename(file_path)
-    docs.append((blob.name, blob.path, file_path))
-  return docs
+  def chunk_document(self, doc_name: str, doc_url: str, doc_filepath: str) -> \
+                      Tuple[List[QueryDocument], List[str]]:
+    """
+    Process docs at url and upload embeddings to GCS for indexing.
+    Returns:
+       Tuple of list of QueryDocument objects for docs processed,
+          list of doc urls of docs not processed
+    """
+
+    text_chunks = None
+
+    # use langchain text splitter
+    text_splitter = CharacterTextSplitter(chunk_size=CHUNK_SIZE,
+                                          chunk_overlap=0)
+
+    Logger.info(f"generating index data for {doc_name}")
+
+    # read doc data and split into text chunks
+    # skip any file that can't be read or generates an error
+    doc_text_list = None
+    try:
+      doc_text_list = read_doc(doc_name, doc_filepath)
+      if doc_text_list is None:
+        Logger.error(f"no content read from {doc_name}")
+        self.docs_not_processed.append(doc_url)
+    except Exception as e:
+      Logger.error(f"error reading doc {doc_name}: {e}")
+      self.docs_not_processed.append(doc_url)  
+  
+    if doc_text_list is not None:
+      # split text into chunks
+      text_chunks = []
+      for text in doc_text_list:
+        text_chunks.extend(text_splitter.split_text(text))
+
+      if all(element == "" for element in text_chunks):
+        Logger.warning(f"All extracted pages from {doc_name} are empty.")
+        self.docs_not_processed.append(doc_url)
+
+    return text_chunks
 
 
-def read_doc(doc_name: str, doc_filepath: str) -> List[str]:
-  """ Read document and return content as a list of strings """
-  doc_extension = doc_name.split(".")[-1]
-  doc_extension = doc_extension.lower()
-  doc_text_list = None
-  loader = None
+  def download_files_to_local(self, local_dir,
+                              doc_url: str) -> List[Tuple[str, str, str]]:
+    """ Download files from GCS to a local tmp directory """
+    docs = []
+    bucket_name = doc_url.split("gs://")[1].split("/")[0]
+    Logger.info(f"downloading {doc_url} from bucket {bucket_name}")
+    for blob in self.storage_client.list_blobs(bucket_name):
+      # Download the file to the tmp folder flattening all directories
+      file_name = Path(blob.name).name
+      file_path = os.path.join(local_dir, file_name)
+      blob.download_to_filename(file_path)
+      docs.append((blob.name, blob.path, file_path))
+    return docs
 
-  if doc_extension == "txt":
-    with open(doc_filepath, "r", encoding="utf-8") as f:
-      doc_text = f.read()
-    doc_text_list = [doc_text]
-  elif doc_extension == "csv":
-    loader = CSVLoader(file_path=doc_filepath)
-  elif doc_extension == "pdf":
-    # read PDF into array of pages
-    doc_text_list = []
-    with open(doc_filepath, "rb") as f:
-      reader = PdfReader(f)
-      num_pages = len(reader.pages)
-      Logger.info(f"Reading pdf file {doc_name} with {num_pages} pages")
-      for page in range(num_pages):
-        doc_text_list.append(reader.pages[page].extract_text())
-      Logger.info(f"Finished reading pdf file {doc_name}")
-  else:
-    # return None if doc type not supported
-    Logger.error(
-        f"Cannot read {doc_name}: unsupported extension {doc_extension}")
-    pass
 
-  if loader is not None:
-    langchain_document = loader.load()
-    doc_text_list = [section.content for section in langchain_document]
+  def read_doc(self, doc_name: str, doc_filepath: str) -> List[str]:
+    """ Read document and return content as a list of strings """
+    doc_extension = doc_name.split(".")[-1]
+    doc_extension = doc_extension.lower()
+    doc_text_list = None
+    loader = None
 
-  return doc_text_list
+    if doc_extension == "txt":
+      with open(doc_filepath, "r", encoding="utf-8") as f:
+        doc_text = f.read()
+      doc_text_list = [doc_text]
+    elif doc_extension == "csv":
+      loader = CSVLoader(file_path=doc_filepath)
+    elif doc_extension == "pdf":
+      # read PDF into array of pages
+      doc_text_list = []
+      with open(doc_filepath, "rb") as f:
+        reader = PdfReader(f)
+        num_pages = len(reader.pages)
+        Logger.info(f"Reading pdf file {doc_name} with {num_pages} pages")
+        for page in range(num_pages):
+          doc_text_list.append(reader.pages[page].extract_text())
+        Logger.info(f"Finished reading pdf file {doc_name}")
+    else:
+      # return None if doc type not supported
+      Logger.error(
+          f"Cannot read {doc_name}: unsupported extension {doc_extension}")
+      pass
+
+    if loader is not None:
+      langchain_document = loader.load()
+      doc_text_list = [section.content for section in langchain_document]
+
+    return doc_text_list
