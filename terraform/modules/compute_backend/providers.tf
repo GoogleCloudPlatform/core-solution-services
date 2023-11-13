8fde469e2e2caf60ea2ff2be95f3101bd8f20306
Lukman Ramsey <lramsey@google.com>
2023-11-13 11:45:35 -0500

    terraform fixes / provider version updates (#187)

--- terraform/modules/compute_backend/providers.tf
+++ terraform/modules/compute_backend/providers.tf
@@ -20,11 +20,11 @@ terraform {
 
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.50, < 5.0"
+      version = ">= 4.50"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 4.50, < 5.0"
+      version = ">= 4.50"
     }
     random = {
       source  = "hashicorp/random"
