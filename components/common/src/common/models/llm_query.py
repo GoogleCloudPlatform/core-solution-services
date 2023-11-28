79efe9ac19582b422fe8b93cbe63bac1f152bfb1
Jon Chen <jonchen@google.com>
2023-11-28 14:18:26 -0500

    removing raw_text in QueryDocumentChunk

--- components/common/src/common/models/llm_query.py
+++ components/common/src/common/models/llm_query.py
@@ -206,7 +206,6 @@ class QueryDocumentChunk(BaseModel):
   query_document_id = TextField(required=True)
   index = NumberField(required=True)
   text = TextField(required=True)
-  raw_text = TextField(required=False)
   clean_text = TextField(required=False)
   sentences = ListField(required=False)
 
