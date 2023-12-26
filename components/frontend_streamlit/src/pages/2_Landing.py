961f3279d735bcce87e996d868edf9b2793d87ab
Stephen Magrowski <stephen.magrowski@gmail.com>
2023-12-26 11:34:43 -0500

    Update 2_Landing.py

--- components/frontend_streamlit/src/pages/2_Landing.py
+++ components/frontend_streamlit/src/pages/2_Landing.py
@@ -16,14 +16,22 @@
 """
 # pylint: disable=invalid-name,pointless-string-statement,unused-variable
 import streamlit as st
+from api import get_all_query_engines, get_all_chat_llm_types
 from components.chat_history import chat_history_panel
-from components.landing_header import display_header
-from components.suggestions import landing_suggestions
-from styles.landing_markup import landing_theme
 from common.utils.logging_handler import Logger
 import utils
 
 Logger = Logger.get_logger(__file__)
+
+
+LANDING_PAGE_STYLES = """
+<style>
+  .stButton[data-testid="stFormSubmitButton"] {
+    display: none;
+  }
+</style>
+"""
+
 
 def landing_page():
   """
@@ -34,33 +42,71 @@ def landing_page():
   # Clean up session.
   utils.reset_session_state()
 
-  # Returns the values of the select input boxes
-  selections = display_header()
+  chat_llm_types = get_all_chat_llm_types()
 
-  st.title("Hello again")
-  st.subheader("Start your journey here or explore the options below.")
-  landing_theme()
-  # Center content
-  landing_suggestions()
+  st.markdown(LANDING_PAGE_STYLES, unsafe_allow_html=True)
+
+  st.title("Hello again.")
+  st.subheader("You can ask me anything:")
 
   with st.form("user_input_form", border=False, clear_on_submit=True):
-    input_col, btn_col = st.columns([9.4, .6])
-
-    with input_col:
-      user_input = st.text_input(
-        placeholder="Enter a prompt here",
-        label="Enter prompt",
-        label_visibility="collapsed",
-        key="landing_input"
-      )
-    
-    with btn_col:
+    col1, col2, col3 = st.columns([5, 3, 2])
+    with col1:
+      user_input = st.text_input("")
       submitted = st.form_submit_button("Submit")
+    with col2:
+      st.session_state.chat_llm_type = st.selectbox(
+          "Model", chat_llm_types)
+    with col3:
+      st.session_state.default_route = st.selectbox(
+          "Chat Mode", ["Auto", "Chat", "Plan", "Query"])
 
     if submitted:
       utils.reset_session_state()
       st.session_state.landing_user_input = user_input
       utils.navigate_to("Chat")
+
+  st.divider()
+
+  st.subheader("Or run with a specific Agent or Task:")
+  start_chat, start_query = st.columns((1, 1))
+
+  with start_chat:
+    with st.container():
+      agent_name = st.selectbox(
+          "Agent:",
+          ("Chat", "Plan"))
+      chat_button = st.button("Start", key=2)
+      if chat_button:
+        st.session_state.agent_name = agent_name
+        st.session_state.chat_id = None
+        utils.navigate_to("Agent")
+
+  with start_query:
+    # Get all query engines as a list
+    query_engine_list = get_all_query_engines(
+      auth_token=st.session_state.auth_token)
+
+    if query_engine_list is None or len(query_engine_list) == 0:
+      with st.container():
+        "No Query Engines"
+    else:
+      query_engines = {}
+      for item in query_engine_list:
+        query_engines[item["name"]] = item
+
+      Logger.info(query_engines)
+      with st.container():
+        qe_name = st.selectbox(
+            "Query Engine:",
+            tuple(query_engines.keys()))
+        query_button = st.button("Start", key=3)
+        query_engine_id = query_engines[qe_name]["id"]
+        st.session_state.query_engine_id = query_engine_id
+        if query_button:
+          st.session_state.agent_name = agent_name
+          st.session_state.chat_id = None
+          utils.navigate_to("Query")
 
 if __name__ == "__main__":
   utils.init_page()
