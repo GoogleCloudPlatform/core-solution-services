b7044d251a9866c20ca34da38eb5f1b82a6b7269
Lukman Ramsey <lramsey@google.com>
2024-01-27 12:15:22 -0500

    test_plan_route passing

--- components/llm_service/src/services/agents/routing_agent_test.py
+++ components/llm_service/src/services/agents/routing_agent_test.py
@@ -23,7 +23,8 @@ import pytest
 import tempfile
 from unittest import mock
 from config.utils import set_agent_config
-from common.models import User, UserChat, QueryResult, QueryEngine
+from common.models import (User, UserChat, QueryResult,
+                           QueryEngine, UserPlan, PlanStep)
 from common.models.agent import AgentCapability
 from common.utils.logging_handler import Logger
 from config import (get_model_config, PROVIDER_LANGCHAIN,
@@ -35,7 +36,10 @@ from schemas.schema_examples import (CHAT_EXAMPLE,
                                      USER_EXAMPLE,
                                      QUERY_RESULT_EXAMPLE,
                                      QUERY_ENGINE_EXAMPLE,
-                                     USER_QUERY_EXAMPLE)
+                                     USER_QUERY_EXAMPLE,
+                                     USER_PLAN_EXAMPLE,
+                                     USER_PLAN_STEPS_EXAMPLE_1,
+                                     USER_PLAN_STEPS_EXAMPLE_2)
 from common.testing.firestore_emulator import firestore_emulator, clean_firestore
 from services.agents.routing_agent import run_intent, run_routing_agent
 
@@ -50,11 +54,11 @@ FAKE_QUERY_ROUTE = f"Query:{QUERY_ENGINE_EXAMPLE['name']}"
 FAKE_PLAN_ROUTE = "Plan"
 FAKE_DB_ROUTE = "Database"
 
-FAKE_AGENT_LOGS = ""
+FAKE_AGENT_LOGS = "fake logs"
+FAKE_AGENT_OUTPUT = "fake agent output"
 FAKE_ROUTING_AGENT = "Router"
 
 FAKE_DB_AGENT_RESULT = {}
-FAKE_PLAN_AGENT_RESULT = {}
 
 TEST_AGENT_CONFIG = {
   "Agents":
@@ -120,6 +124,17 @@ def create_query_engine(firestore_emulator, clean_firestore):
   return q_engine
 
 @pytest.fixture
+def create_plan(clean_firestore):
+  plan_dict = USER_PLAN_EXAMPLE
+  plan = UserPlan.from_dict(plan_dict)
+  plan.save()
+  plan_step = PlanStep.from_dict(USER_PLAN_STEPS_EXAMPLE_1)
+  plan_step.save()
+  plan_step = PlanStep.from_dict(USER_PLAN_STEPS_EXAMPLE_2)
+  plan_step.save()
+  return plan
+
+@pytest.fixture
 def test_model_config(firestore_emulator, clean_firestore):
   get_model_config().llm_model_providers = {
     PROVIDER_LANGCHAIN: TEST_OPENAI_CONFIG
@@ -157,7 +172,7 @@ async def test_query_route(mock_run_intent,
 
   agent_name = FAKE_ROUTING_AGENT
   prompt = "when does a chicken start laying eggs?"
-    
+
   route, response_data = await run_routing_agent(
       prompt, agent_name, create_user, create_chat)
 
@@ -167,6 +182,7 @@ async def test_query_route(mock_run_intent,
   assert response_data["output"] == create_query_result.response
   assert response_data["query_engine_id"] == create_query_engine.id
   assert response_data["query_references"] == FAKE_REFERENCES
+  assert response_data["agent_logs"] == FAKE_AGENT_LOGS
 
 
 @pytest.mark.asyncio
@@ -174,26 +190,33 @@ async def test_query_route(mock_run_intent,
 @mock.patch("services.agents.routing_agent.agent_plan")
 @mock.patch("services.agents.routing_agent.run_intent")
 async def test_plan_route(mock_run_intent,
-                           mock_agent_plan,
-                           mock_get_agent_config,
-                           test_model_config,
-                           test_agent_config,                           
-                           create_user, create_chat):
+                          mock_agent_plan,
+                          mock_get_agent_config,
+                          test_model_config,
+                          test_agent_config,
+                          create_user, create_chat, create_plan):
   """ Test run_routing_agent with plan route """
 
+  fake_plan_result = create_plan.get_fields(reformat_datetime=True)
+  fake_plan_result["id"] = create_plan.id
+
   mock_run_intent.return_value = FAKE_PLAN_ROUTE, FAKE_AGENT_LOGS
-  mock_agent_plan.return_value = FAKE_PLAN_AGENT_RESULT
+  mock_agent_plan.return_value = FAKE_AGENT_OUTPUT, create_plan
   mock_get_agent_config.return_value = TEST_AGENT_CONFIG
 
   agent_name = FAKE_ROUTING_AGENT
-  prompt = "when does a chicken start laying eggs?"
-    
+  prompt = "make a plan to get some chickens"
+
   route, response_data = await run_routing_agent(
       prompt, agent_name, create_user, create_chat)
-#
-  #assert route == AgentCapability.QUERY.value
-  
-  #assert response_data["resources"]["Spreadsheet"] == "test url"
+
+  assert route == AgentCapability.PLAN.value
+  assert response_data["route"] == AgentCapability.PLAN.value
+  assert response_data["route_name"] == FAKE_PLAN_ROUTE
+  assert response_data["content"] == FAKE_AGENT_OUTPUT
+  assert response_data["plan"] == fake_plan_result
+  assert response_data["agent_logs"] == FAKE_AGENT_OUTPUT
+
 
 @pytest.mark.asyncio
 @mock.patch("config.utils.get_agent_config")
@@ -213,12 +236,12 @@ async def test_db_route(mock_run_intent,
 
   agent_name = FAKE_ROUTING_AGENT
   prompt = "when does a chicken start laying eggs?"
-    
+
   route, response_data = await run_routing_agent(
       prompt, agent_name, create_user, create_chat)
 
 #
   #assert route == AgentCapability.QUERY.value
-  
+
   #assert response_data["resources"]["Spreadsheet"] == "test url"
 
