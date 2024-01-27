3116ceb7a32a90cf49de7306f5f09704edc03a7c
Lukman Ramsey <lramsey@google.com>
2024-01-26 23:32:51 -0500

    refactor agent config load to allow setting config from a dict

--- components/llm_service/src/config/utils.py
+++ components/llm_service/src/config/utils.py
@@ -39,52 +39,61 @@ def get_dataset_config() -> dict:
 
 def load_agent_config(agent_config_path: str):
   """
-  Load and validate agent config
+  Load agent config from a filepath
   """
-  global AGENTS
   try:
     agent_config = load_config_json(agent_config_path)
-    agent_config = agent_config.get("Agents")
-
-    # add agent class and capabilities
-    from services.agents import agents
-    agent_classes = {
-      k:klass for (k, klass) in inspect.getmembers(agents)
-      if isinstance(klass, type)
-    }
-    # update agent config dict for each agent
-    for values in agent_config.values():
-      agent_class_val = values["agent_class"]
-      agent_class = agent_classes.get(agent_class_val)
-      if agent_class is None:
-        raise RuntimeError(f"Cannot find agent class {agent_class_val}")
-      values["agent_class"] = agent_class
-      values["capabilities"] = [c.value for c in agent_class.capabilities()]
-
-    # validate config according to Agent model
-    # for each agent, save or update model according to config
-    for agent_name, ac_dict in agent_config.items():
-      agent_model = Agent.find_by_name(agent_name)
-      if not agent_model:
-        agent_model = Agent()
-      agent_model.name = agent_name
-      agent_model.tools = get_config_list(ac_dict.get("tools"))
-      agent_model.llm_type = ac_dict.get("llm_type")
-      agent_model.capabilities = ac_dict["capabilities"]
-      agent_model.agent_type = ac_dict.get("agent_type")
-
-      # validate agent model in case config is invalid
-      valid, validation_errors = agent_model.validate()
-      if not valid:
-        raise RuntimeError(
-            f"Invalid Agent config for {agent_name}: {validation_errors}")
-
-      # update agent model
-      agent_model.save(merge=True)
-
-    AGENTS = agent_config
+    set_agent_config(agent_config)
   except Exception as e:
-    raise RuntimeError(f" Error loading agent config: {e}") from e
+    raise RuntimeError(f" Error loading agent config: {e}") from e    
+
+def set_agent_config(agent_config: dict):
+  """
+  Validate agent config and update agent models
+  """
+  global AGENTS
+
+  agent_config = agent_config.get("Agents", None)
+  if agent_config is None:
+    raise RuntimeError(f"Missing agent config")
+
+  # add agent class and capabilities
+  from services.agents import agents
+  agent_classes = {
+    k:klass for (k, klass) in inspect.getmembers(agents)
+    if isinstance(klass, type)
+  }
+  # update agent config dict for each agent
+  for values in agent_config.values():
+    agent_class_val = values["agent_class"]
+    agent_class = agent_classes.get(agent_class_val)
+    if agent_class is None:
+      raise RuntimeError(f"Cannot find agent class {agent_class_val}")
+    values["agent_class"] = agent_class
+    values["capabilities"] = [c.value for c in agent_class.capabilities()]
+
+  # validate config according to Agent model
+  # for each agent, save or update model according to config
+  for agent_name, ac_dict in agent_config.items():
+    agent_model = Agent.find_by_name(agent_name)
+    if not agent_model:
+      agent_model = Agent()
+    agent_model.name = agent_name
+    agent_model.tools = get_config_list(ac_dict.get("tools"))
+    agent_model.llm_type = ac_dict.get("llm_type")
+    agent_model.capabilities = ac_dict["capabilities"]
+    agent_model.agent_type = ac_dict.get("agent_type")
+
+    # validate agent model in case config is invalid
+    valid, validation_errors = agent_model.validate()
+    if not valid:
+      raise RuntimeError(
+          f"Invalid Agent config for {agent_name}: {validation_errors}")
+
+    # update agent model
+    agent_model.save(merge=True)
+
+  AGENTS = agent_config
 
 def get_agent_config() -> dict:
   if AGENTS is None:
