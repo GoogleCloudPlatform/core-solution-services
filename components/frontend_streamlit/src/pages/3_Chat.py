5250eb177c6159937417e6e6f5088ad337016eb2
Jon Chen <jonchen@google.com>
2024-02-06 20:15:45 -0500

    Update streamlit Chat to handle async call, update routing_agent to update chat history

--- components/frontend_streamlit/src/pages/3_Chat.py
+++ components/frontend_streamlit/src/pages/3_Chat.py
@@ -67,70 +67,80 @@ REFERENCE_CSS_STYLE = """
 """
 
 
+content_container = None
+spinner_container = None
+on_submit_clicked = False
+
 def on_submit(user_input):
   """ Run dispatch agent when adding an user input prompt """
-  # Appending messages.
   st.session_state.error_msg = None
   st.session_state.messages.append({"HumanInput": user_input})
+  message_index = len(st.session_state.messages)
 
-  with st.spinner("Loading..."):
-    # Send API to llm-service
-    default_route = st.session_state.get("default_route", None)
-    routing_agents = get_all_routing_agents()
-    routing_agent_names = list(routing_agents.keys())
-    chat_llm_type = st.session_state.get("chat_llm_type")
-    Logger.info(f"llm_type in session {chat_llm_type}")
+  with st.chat_message("user"):
+    st.write(user_input, is_user=True, key=f"human_{message_index}")
 
-    if default_route is None:
-      # pick the first routing agent as default
-      if routing_agent_names:
-        routing_agent = routing_agent_names[0]
-      else:
-        routing_agent = "default"
-      response = run_dispatch(user_input,
-                              routing_agent,
-                              chat_id=st.session_state.get("chat_id"),
-                              llm_type=chat_llm_type,
-                              run_as_batch_job=True)
-      st.session_state.default_route = response.get("route", None)
+  # Send API to llm-service
+  default_route = st.session_state.get("default_route", None)
+  routing_agents = get_all_routing_agents()
+  routing_agent_names = list(routing_agents.keys())
+  chat_llm_type = st.session_state.get("chat_llm_type")
+  Logger.info(f"llm_type in session {chat_llm_type}")
 
-    elif default_route in routing_agent_names:
-      response = run_dispatch(user_input,
-                              default_route,
-                              chat_id=st.session_state.get("chat_id"),
-                              llm_type=chat_llm_type,
-                              run_as_batch_job=True)
-      st.session_state.default_route = response.get("route", None)
-
-    elif default_route == "Chat":
-      response = run_chat(user_input,
-                          chat_id=st.session_state.get("chat_id"),
-                          llm_type=chat_llm_type)
-    elif default_route == "Plan":
-      response = run_agent_plan("Plan", user_input,
-                                chat_id=st.session_state.get("chat_id"),
-                                llm_type=chat_llm_type)
+  if default_route is None:
+    # pick the first routing agent as default
+    if routing_agent_names:
+      routing_agent = routing_agent_names[0]
     else:
-      st.error(f"Unsupported route {default_route}")
-      response = None
+      routing_agent = "default"
+    response = run_dispatch(user_input,
+                            routing_agent,
+                            chat_id=st.session_state.get("chat_id"),
+                            llm_type=chat_llm_type,
+                            run_as_batch_job=True)
+    st.session_state.default_route = response.get("route", None)
 
-    if response:
-      st.session_state.chat_id = response["chat"]["id"]
-      st.session_state.user_chats.insert(0, response["chat"])
+  elif default_route in routing_agent_names:
+    response = run_dispatch(user_input,
+                            default_route,
+                            chat_id=st.session_state.get("chat_id"),
+                            llm_type=chat_llm_type,
+                            run_as_batch_job=True)
+    st.session_state.default_route = response.get("route", None)
 
-      # TODO: Currently the AIOutput vs content are inconsistent across
-      # API response and in a UserChat history.
-      if "content" in response:
-        response["AIOutput"] = response["content"]
-      del response["chat"]
-      st.session_state.messages.append(response)
+  elif default_route == "Chat":
+    response = run_chat(user_input,
+                        chat_id=st.session_state.get("chat_id"),
+                        llm_type=chat_llm_type)
+  elif default_route == "Plan":
+    response = run_agent_plan("Plan", user_input,
+                              chat_id=st.session_state.get("chat_id"),
+                              llm_type=chat_llm_type)
+  else:
+    st.error(f"Unsupported route {default_route}")
+    response = None
 
-      if "batch_job" in response:
-        update_async_job(response["batch_job"]["id"])
+  if response:
+    st.session_state.chat_id = response["chat"]["id"]
+    st.session_state.user_chats.insert(0, response["chat"])
 
-  # reload page after exiting from spinner
-  utils.navigate_to("Chat")
+    # TODO: Currently the AIOutput vs content are inconsistent across
+    # API response and in a UserChat history.
+    if "content" in response:
+      response["AIOutput"] = response["content"]
+    del response["chat"]
 
+    # Append new message from the API response and display it.
+    append_and_display_message(response)
+
+    # If the response has a batch async job, keep pulling the job result.
+    if "batch_job" in response:
+      update_async_job(response["batch_job"]["id"], loop_seconds=1)
+
+def hide_loading():
+  global spinner_container
+  with spinner_container:
+    st.write("")
 
 def format_ai_output(text):
   text = text.strip()
@@ -160,7 +170,7 @@ def dedup_list(items, dedup_key):
   return list(items_dict.values())
 
 
-def chat_content():
+def chat_metadata():
   if st.session_state.debug:
     with st.expander("DEBUG: session_state"):
       st.write(st.session_state.get("landing_user_input"))
@@ -169,162 +179,47 @@ def chat_content():
   if st.session_state.chat_id:
     st.write(f"Chat ID: **{st.session_state.chat_id}**")
 
-  # Create a placeholder for all chat history.
-  chat_placeholder = st.empty()
-  with chat_placeholder.container():
-    index = 1
-    for item in st.session_state.messages:
-      Logger.info(item)
 
-      if "HumanInput" in item:
-        with st.chat_message("user"):
-          st.write(item["HumanInput"], is_user=True, key=f"human_{index}")
-
-      if "route_name" in item:
-        route_name = item["route_name"]
-        with st.chat_message("ai"):
-          st.write(
-              f"Using route **`{route_name}`** to respond.",
-              key=f"ai_{index}",
-          )
-
-      route_logs = item.get("route_logs", None)
-      if route_logs and route_logs.strip() != "":
-        with st.expander("Expand to see Agent's thought process"):
-          st.write(format_ai_output(route_logs))
-
-      if "AIOutput" in item:
-        with st.chat_message("ai"):
-          ai_output = item["AIOutput"]
-          ai_output = format_ai_output(ai_output)
-          st.write(
-              ai_output,
-              key=f"ai_{index}",
-              unsafe_allow_html=False,
-              is_table=False,  # TODO: Detect whether an output content type.
-          )
-
-      # Append all query references.
-      if item.get("db_result", None):
-        with st.chat_message("ai"):
-          st.write("Query result:")
-          result_index = 1
-
-          # Clean up empty rows.
-          db_result = []
-          for result in item["db_result"]:
-            if len(result.keys()) > 0:
-              db_result.append(result)
-
-          if len(db_result) > 0:
-            for result in db_result:
-              values = [str(x) for x in list(result.values())]
-              if len(values) > 0:
-                markdown_content = f"{result_index}. **{values[0]}**"
-                markdown_content += " - " + ", ".join(values[1:])
-                with stylable_container(
-                  key=f"ref_{result_index}",
-                  css_styles=REFERENCE_CSS_STYLE
-                ):
-                  st.markdown(markdown_content)
-              result_index = result_index + 1
-
-          else:
-            with stylable_container(
-              key=f"ref_{result_index}",
-              css_styles=REFERENCE_CSS_STYLE
-            ):
-              st.markdown("No result found.")
-
-      # Append all resources.
-      if item.get("resources", None):
-        with st.chat_message("ai"):
-          for name, link in item["resources"].items():
-            st.markdown(f"Resource: [{name}]({link})")
-
-      # Append all query references.
-      if item.get("query_references", None):
-        with st.chat_message("ai"):
-          st.write("References:")
-          reference_index = 1
-          for reference in dedup_list(item["query_references"], "chunk_id"):
-            document_url = render_cloud_storage_url(reference["document_url"])
-            document_text = reference["document_text"]
-            st.markdown(
-                f"**{reference_index}.** [{document_url}]({document_url})")
-            markdown_content = re.sub(
-                r"<b>(.*?)</b>", r"**\1**", document_text, flags=re.IGNORECASE)
-
-            with stylable_container(
-              key=f"ref_{reference_index}",
-              css_styles=REFERENCE_CSS_STYLE
-            ):
-              st.markdown(markdown_content)
-
-            reference_index = reference_index + 1
-          st.divider()
-
-      if "plan" in item:
-        with st.chat_message("ai"):
-          index = 1
-
-          plan = get_plan(item["plan"]["id"])
-          Logger.info(plan)
-
-          for step in plan["plan_steps"]:
-            st.text_area(f"step-{index}", step["description"],
-                         height=30, disabled=True,
-                         label_visibility="hidden")
-            index = index + 1
-
-          plan_id = plan["id"]
-          if st.button("Execute this plan", key=f"plan-{plan_id}"):
-            with st.spinner("Executing the plan..."):
-              output = run_agent_execute_plan(
-                plan_id=plan_id,
-                chat_id=st.session_state.chat_id,
-                auth_token=st.session_state.auth_token)
-            st.session_state.messages.append({
-              "AIOutput": f"Plan executed successfully. (plan_id={plan_id})",
-            })
-
-            agent_process_output = output.get("agent_process_output", "")
-            agent_process_output = ansi_escape.sub("", agent_process_output)
-            st.session_state.messages.append({
-              "AIOutput": agent_process_output,
-            })
-
-      agent_logs = item.get("agent_logs", None)
-      if agent_logs and agent_logs.strip() != "":
-        with st.expander("Expand to see Agent's thought process"):
-          st.write(format_ai_output(agent_logs))
-
-      index = index + 1
-
-
-def update_async_job(job_id):
+def update_async_job(job_id, loop_seconds=3, timeout_seconds=120):
+  global spinner_container
   start_time = time.time()
-  timeout = 120 # timeout seconds
 
-  while (time.time() - start_time) < timeout:
+  with spinner_container:
+    with st.chat_message("ai"):
+      st.write("Loading...", is_user=True, key=f"ai_loading")
+
+  while (time.time() - start_time) < timeout_seconds:
     job = get_job(JOB_TYPE_ROUTING_AGENT, job_id)
-    if st.session_state.debug:
-      st.write(f"Waiting for job: {job_id}, status={job['status']}")
-    init_messages()
+    with content_container:
+      # Pull the latest chat history.
+      new_messages = append_new_messages()
+      if len(new_messages) > 0:
+        st.write(f"Found {len(new_messages)} new messages!")
+        st.write(new_messages)
 
+    # Breaks if the batch job doesn't exist.
     if not job:
       return
 
     # Refresh messages when job status is "succeeded" or "failed".
     if job["status"] == JobStatus.JOB_STATUS_SUCCEEDED.value:
-      init_messages()
+      # append_and_display_message(job["result_data"])
+      hide_loading()
+      append_new_messages()
       return
 
     elif job["status"] == JobStatus.JOB_STATUS_FAILED.value:
+      hide_loading()
       st.write("Job failed.")
       return
 
-    time.sleep(3)
+    time.sleep(loop_seconds)
+
+  # Timeout
+  display_message({
+    "AIOutput": f"Timed out after {timeout_seconds} seconds."
+  }, len(st.session_state.messages))
+
 
 def render_cloud_storage_url(url):
   """ Parse a cloud storage url. """
@@ -340,55 +235,207 @@ def init_messages():
     chat_data = get_chat(st.session_state.chat_id)
     st.session_state.messages = chat_data["history"]
   elif not st.session_state.get("messages", None):
-    st.session_state.messages = [{
+    display_message({
       "AIOutput": "You can ask me anything."
-    }]
+    }, 0)
+    st.session_state.messages = []
+
+  index = 1
+  for message in st.session_state.messages:
+    display_message(message, index)
+    index += 1
+
+
+def append_new_messages():
+  chat_data = get_chat(st.session_state.chat_id)
+  new_messages = chat_data["history"][len(st.session_state.messages):]
+  for item in new_messages:
+    append_and_display_message(item)
+
+  return new_messages
+
+
+def append_and_display_message(item):
+  st.session_state.messages.append(item)
+  display_message(item, len(st.session_state.messages))
+
+
+def display_message(item, item_index):
+  if "HumanInput" in item:
+    with st.chat_message("user"):
+      st.write(item["HumanInput"], is_user=True, key=f"human_{item_index}")
+
+  if "route_name" in item and "AIOutput" not in item:
+    route_name = item["route_name"]
+    with st.chat_message("ai"):
+      st.write(
+          f"Using route **`{route_name}`** to respond.",
+          key=f"ai_{item_index}",
+      )
+
+  route_logs = item.get("route_logs", None)
+  if route_logs and route_logs.strip() != "":
+    with st.expander("Expand to see Agent's thought process"):
+      st.write(format_ai_output(route_logs))
+
+  if "AIOutput" in item:
+    with st.chat_message("ai"):
+      ai_output = item["AIOutput"]
+      ai_output = format_ai_output(ai_output)
+      st.write(
+          ai_output,
+          key=f"ai_{item_index}",
+          unsafe_allow_html=False,
+          is_table=False,  # TODO: Detect whether an output content type.
+      )
+
+  # Append all query references.
+  if item.get("db_result", None):
+    with st.chat_message("ai"):
+      st.write("Query result:")
+      result_index = 1
+
+      # Clean up empty rows.
+      db_result = []
+      for result in item["db_result"]:
+        if len(result.keys()) > 0:
+          db_result.append(result)
+
+      if len(db_result) > 0:
+        for result in db_result:
+          values = [str(x) for x in list(result.values())]
+          if len(values) > 0:
+            markdown_content = f"{result_index}. **{values[0]}**"
+            markdown_content += " - " + ", ".join(values[1:])
+            with stylable_container(
+              key=f"ref_{result_index}",
+              css_styles=REFERENCE_CSS_STYLE
+            ):
+              st.markdown(markdown_content)
+          result_index = result_index + 1
+
+      else:
+        with stylable_container(
+          key=f"ref_{result_index}",
+          css_styles=REFERENCE_CSS_STYLE
+        ):
+          st.markdown("No result found.")
+
+  # Append all resources.
+  if item.get("resources", None):
+    with st.chat_message("ai"):
+      for name, link in item["resources"].items():
+        st.markdown(f"Resource: [{name}]({link})")
+
+  # Append all query references.
+  if item.get("query_references", None):
+    with st.chat_message("ai"):
+      st.write("References:")
+      reference_index = 1
+      for reference in dedup_list(item["query_references"], "chunk_id"):
+        document_url = render_cloud_storage_url(reference["document_url"])
+        document_text = reference["document_text"]
+        st.markdown(
+            f"**{reference_index}.** [{document_url}]({document_url})")
+        markdown_content = re.sub(
+            r"<b>(.*?)</b>", r"**\1**", document_text, flags=re.IGNORECASE)
+
+        with stylable_container(
+          key=f"ref_{reference_index}",
+          css_styles=REFERENCE_CSS_STYLE
+        ):
+          st.markdown(markdown_content)
+
+        reference_index = reference_index + 1
+      st.divider()
+
+  if "plan" in item:
+    with st.chat_message("ai"):
+      plan_index = 1
+
+      plan = get_plan(item["plan"]["id"])
+      Logger.info(plan)
+
+      for step in plan["plan_steps"]:
+        st.text_area(f"step-{item_index}", step["description"],
+                      height=30, disabled=True,
+                      label_visibility="hidden")
+        plan_index += 1
+
+      plan_id = plan["id"]
+      if st.button("Execute this plan", key=f"plan-{plan_id}"):
+        with st.spinner("Executing the plan..."):
+          output = run_agent_execute_plan(
+            plan_id=plan_id,
+            chat_id=st.session_state.chat_id,
+            auth_token=st.session_state.auth_token)
+        st.session_state.messages.append({
+          "AIOutput": f"Plan executed successfully. (plan_id={plan_id})",
+        })
+
+        agent_process_output = output.get("agent_process_output", "")
+        agent_process_output = ansi_escape.sub("", agent_process_output)
+        st.session_state.messages.append({
+          "AIOutput": agent_process_output,
+        })
+
+  agent_logs = item.get("agent_logs", None)
+  if agent_logs and agent_logs.strip() != "":
+    with st.expander("Expand to see Agent's thought process"):
+      st.write(format_ai_output(agent_logs))
+
+  item_index += 1
 
 
 def chat_page():
+  global content_container
+  global on_submit_clicked
+  global spinner_container
+
   chat_theme()
 
   # display chat header
   chat_header(refresh_func=init_messages)
 
   st.title("Chat")
+  chat_metadata()
 
   # List all existing chats if any. (data model: UserChat)
   chat_history_panel()
 
-  content_placeholder = st.container()
+  content_container = st.container()
+  spinner_container = st.empty()
 
-  # Pass prompt from the Landing page if any.
-  landing_user_input = st.session_state.get("landing_user_input", None)
-  Logger.info(f"Landing input [{landing_user_input}]")
+  with content_container:
+    init_messages()
 
-  if not st.session_state.chat_id and landing_user_input:
-    user_input = st.session_state.landing_user_input
-    st.session_state.user_input = user_input
-    st.session_state.landing_user_input = None
-    on_submit(user_input)
+    # Pass prompt from the Landing page if any.
+    landing_user_input = st.session_state.get("landing_user_input", None)
+    Logger.info(f"Landing input [{landing_user_input}]")
 
-  with st.form("user_input_form", border=False, clear_on_submit=True):
-    input_col, btn_col = st.columns([9.4, .6])
-
-    with input_col:
-      user_input = st.text_input(
-        placeholder="Enter a prompt here",
-        label="Enter prompt",
-        label_visibility="collapsed",
-        key="user_input"
-      )
-    with btn_col:
-      submitted = st.form_submit_button("Submit")
-
-    if submitted:
+    if not st.session_state.chat_id and landing_user_input:
+      user_input = st.session_state.landing_user_input
+      st.session_state.user_input = user_input
+      st.session_state.landing_user_input = None
       on_submit(user_input)
 
-  with content_placeholder:
-    chat_content()
+    with st.form("user_input_form", border=False, clear_on_submit=True):
+      input_col, button_col = st.columns([9.4, .6])
+
+      with input_col:
+        user_input = st.text_input(
+          placeholder="Enter a prompt here",
+          label="Enter prompt",
+          label_visibility="collapsed",
+          key="user_input"
+        )
+      with button_col:
+        on_submit_clicked = st.form_submit_button("Submit")
+
+    if on_submit_clicked:
+      on_submit(user_input)
 
 
 if __name__ == "__main__":
   utils.init_page()
-  init_messages()
   chat_page()
