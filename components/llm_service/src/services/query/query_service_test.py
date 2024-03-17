01ab9cbc508d4c3f0efcc9c578845f1b4139e30e
Lukman Ramsey <lramsey@google.com>
2024-03-17 18:37:54 -0400

    completed query_service test

--- components/llm_service/src/services/query/query_service_test.py
+++ components/llm_service/src/services/query/query_service_test.py
@@ -11,15 +11,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 """
-  Unit tests for Query Service
+  Unit tests for LLM Service endpoints
 """
-# pylint: disable=unused-argument,redefined-outer-name,unused-import
-import os
+# disabling pylint rules that conflict with pytest fixtures
+# pylint: disable=unused-argument,redefined-outer-name,ungrouped-imports,unused-import
 import pytest
+from typing import List
 from unittest import mock
-from services.query.query_prompts import question_prompt
-from services.query.query_prompt_config import QUESTION_PROMPT
 from schemas.schema_examples import (QUERY_EXAMPLE,
                                      USER_QUERY_EXAMPLE,
                                      USER_EXAMPLE,
@@ -31,19 +31,19 @@ from schemas.schema_examples import (QUERY_EXAMPLE,
                                      QUERY_DOCUMENT_CHUNK_EXAMPLE_2,
                                      QUERY_DOCUMENT_CHUNK_EXAMPLE_3,
                                      QUERY_RESULT_EXAMPLE,
-                                     QUERY_REFERENCE_EXAMPLE_1)
+                                     QUERY_REFERENCE_EXAMPLE_1,
+                                     QUERY_REFERENCE_EXAMPLE_2)
 from common.models import (UserQuery, QueryResult, QueryEngine,
                            User, QueryDocument, QueryDocumentChunk,
                            QueryReference)
 from common.testing.firestore_emulator import firestore_emulator, clean_firestore
-
-os.environ["OPENAI_API_KEY"] = "fake-key"
-os.environ["COHERE_API_KEY"] = "fake-key"
-
-with mock.patch("common.utils.secrets.get_secret"):
-  with mock.patch("langchain.chat_models.ChatOpenAI", new=mock.AsyncMock()):
-    with mock.patch("langchain.llms.Cohere", new=mock.AsyncMock()):
-      from config import DEFAULT_QUERY_CHAT_MODEL
+from services.query.query_service import (query_generate,
+                                          query_search,
+                                          query_engine_build,
+                                          process_documents,
+                                          build_doc_index)
+from services.query.vector_store import VectorStore
+from services.query.data_source import DataSource, DataSourceFile
 
 @pytest.fixture
 def create_query_reference(firestore_emulator, clean_firestore):
@@ -65,35 +65,26 @@ def create_user(client_with_emulator):
   user = User.from_dict(user_dict)
   user.save()
 
-
 @pytest.fixture
 def create_engine(client_with_emulator):
   query_engine_dict = QUERY_ENGINE_EXAMPLE
   q_engine = QueryEngine.from_dict(query_engine_dict)
   q_engine.save()
-
+  return q_engine
 
 @pytest.fixture
 def create_user_query(client_with_emulator):
   query_dict = USER_QUERY_EXAMPLE
   query = UserQuery.from_dict(query_dict)
   query.save()
-
+  return query
 
 @pytest.fixture
 def create_query_result(client_with_emulator):
   query_result_dict = QUERY_RESULT_EXAMPLE
   query_result = QueryResult.from_dict(query_result_dict)
   query_result.save()
-
-
-@pytest.fixture
-def create_query_reference(client_with_emulator):
-  query_reference_dict = QUERY_REFERENCE_EXAMPLE_1
-  query_reference = QueryReference.from_dict(query_reference_dict)
-  query_reference.save()
-  return query_reference
-
+  return query_result
 
 @pytest.fixture
 def create_query_docs(client_with_emulator):
@@ -103,7 +94,7 @@ def create_query_docs(client_with_emulator):
   query_doc2.save()
   query_doc3 = QueryDocument.from_dict(QUERY_DOCUMENT_EXAMPLE_3)
   query_doc3.save()
-
+  return [query_doc1, query_doc2, query_doc3]
 
 @pytest.fixture
 def create_query_doc_chunks(client_with_emulator):
@@ -113,25 +104,138 @@ def create_query_doc_chunks(client_with_emulator):
   qdoc_chunk2.save()
   qdoc_chunk3 = QueryDocumentChunk.from_dict(QUERY_DOCUMENT_CHUNK_EXAMPLE_3)
   qdoc_chunk3.save()
+  return [qdoc_chunk1, qdoc_chunk2, qdoc_chunk3]
 
 FAKE_QUERY_PARAMS = QUERY_EXAMPLE
 
 FAKE_GENERATE_RESPONSE = "test generation"
 
+FAKE_GCS_PATH = "gs://fake-bucket"
+
+DOC_NAME_1 = QUERY_DOCUMENT_EXAMPLE_1["doc_url"].split("/")[1]
+DOC_NAME_2 = QUERY_DOCUMENT_EXAMPLE_2["doc_url"].split("/")[1]
+DOC_NAME_3 = QUERY_DOCUMENT_EXAMPLE_3["doc_url"].split("/")[1]
+DSF1 = DataSourceFile(DOC_NAME_1,
+                      QUERY_DOCUMENT_EXAMPLE_1["doc_url"],
+                      f"/tmp/{DOC_NAME_1}",
+                      f"{FAKE_GCS_PATH}/{DOC_NAME_1}")
+DSF2 = DataSourceFile(DOC_NAME_2,
+                      QUERY_DOCUMENT_EXAMPLE_2["doc_url"],
+                      f"/tmp/{DOC_NAME_2}",
+                      f"{FAKE_GCS_PATH}/{DOC_NAME_2}")
+DSF3 = DataSourceFile(DOC_NAME_3,
+                      QUERY_DOCUMENT_EXAMPLE_3["doc_url"],
+                      f"/tmp/{DOC_NAME_3}",
+                      f"{FAKE_GCS_PATH}/{DOC_NAME_3}")
+
+
+class FakeVectorStore(VectorStore):
+  """ mock vector store class """
+  def __init__(self):
+    pass
+  def init_index(self):
+    pass
+  def index_document(self, doc_name: str, text_chunks: List[str],
+                          index_base: int) -> int:
+    return 0
+  def deploy(self):
+    pass
+  def similarity_search(self, q_engine: QueryEngine,
+                        query_embedding: List[float]) -> List[int]:
+    return [0,1,2]
+
+class FakeDataSource(DataSource):
+  """ mock data source class """
+  def __init__(self):
+    pass
+  def download_documents(self, doc_url: str, temp_dir: str) -> \
+        List[DataSourceFile]:
+    return [DSF1, DSF2, DSF3]
+
+  def chunk_document(self, doc_name: str, doc_url: str,
+                     doc_filepath: str) -> List[str]:
+    if doc_url == QUERY_DOCUMENT_EXAMPLE_1["doc_url"]:
+      chunk_list = QUERY_DOCUMENT_CHUNK_EXAMPLE_1["text"]
+    elif doc_url == QUERY_DOCUMENT_EXAMPLE_2["doc_url"]:
+      chunk_list = QUERY_DOCUMENT_CHUNK_EXAMPLE_2["text"]
+    else:
+      chunk_list = None
+    return chunk_list
 
 @pytest.mark.asyncio
-def test_query_generate(create_query_reference,
-                        create_query_reference_2):
-  pass
+@mock.patch("services.query.query_service.llm_generate.llm_chat")
+@mock.patch("services.query.query_service.query_search")
+def test_query_generate(mock_query_search, mock_llm_chat,
+                        create_engine, create_user, create_query_result,
+                        create_query_reference, create_query_reference_2):
+  prompt = QUERY_EXAMPLE["prompt"]
+  mock_query_search.return_value = [create_query_reference,
+                                    create_query_reference_2]
+  mock_llm_chat.return_value = FAKE_GENERATE_RESPONSE
+  query_result, query_references = \
+      query_generate(create_user.id, prompt, create_engine)
+  assert query_result.query_engine_id == create_engine.id
+  assert query_result.prompt == prompt
+  assert query_result.response == FAKE_GENERATE_RESPONSE
+  assert len(query_references) == 2
+  assert query_references[0] == create_query_reference
+  assert query_references[1] == create_query_reference_2
 
-def test_query_engine_build(create_query_reference,
-                            create_query_reference_2):
-  pass
+@mock.patch("services.query.query_service.embeddings.get_embeddings")
+@mock.patch("services.query.query_service.vector_store_from_query_engine")
+@mock.patch("services.query.query_service.get_top_relevant_sentences")
+def test_query_search(mock_get_top_relevant_sentences,
+                      mock_get_vector_store, mock_get_embeddings,
+                      create_engine, create_query_doc_chunks):
+  qdoc_chunk1 = create_query_doc_chunks[0]
+  qdoc_chunk2 = create_query_doc_chunks[1]
+  qdoc_chunk3 = create_query_doc_chunks[2]
+  mock_get_embeddings.return_value = [0,1,2,3]
+  mock_get_vector_store.return_value = FakeVectorStore()
+  mock_get_top_relevant_sentences.return_value = "test sentence"
+  prompt = QUERY_EXAMPLE["prompt"]
+  query_references = query_search(create_engine, prompt, True)
+  assert len(query_references) == len(create_query_doc_chunks)
+  assert query_references[0].chunk_id == qdoc_chunk1.id
+  assert query_references[1].chunk_id == qdoc_chunk2.id
+  assert query_references[2].chunk_id == qdoc_chunk3.id
 
-def test_process_documents(create_query_reference,
-                           create_query_reference_2):
-  pass
+@mock.patch("services.query.query_service.build_doc_index")
+def test_query_engine_build(mock_build_doc_index, create_query_docs,
+                            create_user):
+  mock_build_doc_index.return_value = (
+      [create_query_docs[0], create_query_docs[1]],
+      [create_query_docs[2]]
+  )
+  doc_url = FAKE_GCS_PATH
+  q_engine, docs_processed, docs_not_processed = \
+      query_engine_build(doc_url, QUERY_ENGINE_EXAMPLE["name"], create_user.id)
+  assert q_engine.created_by == create_user.id
+  assert q_engine.name == QUERY_ENGINE_EXAMPLE["name"]
+  assert q_engine.doc_url == doc_url
+  assert docs_processed == [create_query_docs[0], create_query_docs[1]]
+  assert docs_not_processed == [create_query_docs[2]]
 
-def test_build_doc_index(create_query_reference,
-                         create_query_reference_2):
-  pass
+@mock.patch("services.query.query_service.process_documents")
+def test_build_doc_index(mock_process_documents, create_engine):
+  doc_url = FAKE_GCS_PATH
+  qe_vector_store = FakeVectorStore()
+  mock_process_documents.return_value = (
+      [create_query_docs[0], create_query_docs[1]],
+      [create_query_docs[2]]
+  )
+  docs_processed, docs_not_processed = \
+      build_doc_index(doc_url, create_engine, qe_vector_store)
+  assert docs_processed == [create_query_docs[0], create_query_docs[1]]
+  assert docs_not_processed == [create_query_docs[2]]
+
+@mock.patch("services.query.query_service.datasource_from_url")
+def test_process_documents(mock_get_datasource, create_engine):
+  mock_get_datasource.return_value = FakeDataSource()
+  doc_url = FAKE_GCS_PATH
+  qe_vector_store = FakeVectorStore()
+  docs_processed, docs_not_processed = \
+      process_documents(doc_url, qe_vector_store,
+                      create_engine, None)
+  assert {doc.doc_url for doc in docs_processed} == {DSF1.doc_url, DSF2.doc_url}
+  assert set(docs_not_processed) == {DSF3.doc_url}
