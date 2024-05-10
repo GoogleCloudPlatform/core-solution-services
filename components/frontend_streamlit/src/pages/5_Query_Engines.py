424183e6a7d8e4aa06d82e5cebb4f81bb8950356
Lukman Ramsey <lramsey@google.com>
2024-05-10 15:42:58 -0400

    linting

--- components/frontend_streamlit/src/pages/5_Query_Engines.py
+++ components/frontend_streamlit/src/pages/5_Query_Engines.py
@@ -53,7 +53,7 @@ def submit_build(engine_name:str, engine_type:str, doc_url:str,
       child_engines)
 
     if output.get("success") is True:
-      logging.info(f"job output {output}")
+      logging.info("job output %s" % output)
       job_id = output["data"]["name"]
       st.success(f"Query Engine build job created. Job ID: {job_id}")
     else:
