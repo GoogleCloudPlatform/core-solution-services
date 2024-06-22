71b6547bfdc14951c789ca4b45b4b5d074160008
Lukman Ramsey <lramsey@google.com>
2024-06-22 13:20:24 -0400

    fix query param in LLMQueryModel

--- components/llm_service/src/schemas/llm_schema.py
+++ components/llm_service/src/schemas/llm_schema.py
@@ -159,7 +159,7 @@ class LLMQueryModel(BaseModel):
   llm_type: Optional[str]
   run_as_batch_job: Optional[str]
   rank_sentences: Optional[str]
-  query_filter: Optional[dict]
+  query_filter: Optional[str]
 
   class Config():
     orm_mode = True
