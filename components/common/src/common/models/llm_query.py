5d356e1311f244bdbe5eefc04c02b638e90d1fba
Shelley Cazares <shelleycazares@google.com>
2024-08-21 11:22:30 -0400

    Removed TODOs to save any extracted text from non-text chunks

--- components/common/src/common/models/llm_query.py
+++ components/common/src/common/models/llm_query.py
@@ -195,9 +195,6 @@ class QueryReference(BaseModel):
   It points to a specific chunk of text in one of the indexed documents.
 
   """
-  # TODO: Save text, clean_text, and sentences fields whenever text can be
-  # extracted from a document chunk, even if the document chunk is processed
-  # in multimodal mode
   id = IDField()  # All modalities
   query_engine_id = TextField(required=True)  # All modalities
   query_engine = TextField(required=True)  # All modalities
@@ -358,9 +355,6 @@ class QueryDocumentChunk(BaseModel):
   """
   QueryDocumentChunk ORM class.
   """
-  # TODO: Save text, clean_text, and sentences fields whenever text can be
-  # extracted from a document chunk, even if the document chunk is processed
-  # in multimodal mode
   id = IDField()  # All modalities
   query_engine_id = TextField(required=True)  # All modalities
   query_document_id = TextField(required=True)  # All modalities
