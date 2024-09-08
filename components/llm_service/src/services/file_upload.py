e31f7004c9c62d733956a2b1f1fa82c1fd24d1c6
Lukman Ramsey <lramsey@google.com>
2024-09-08 13:02:00 -0400

    clean up file_upload service

--- components/llm_service/src/services/file_upload.py
+++ components/llm_service/src/services/file_upload.py
@@ -16,15 +16,16 @@
 """ Services for multi modal file uploads """
 import os
 import tempfile
-import traceback
 from urllib.parse import urlparse
 from common.utils.logging_handler import Logger
-from common.utils.errors import (ValidationError, UnsupportedError)
+from common.utils.errors import ValidationError, UnsupportedError
 from config import PROJECT_ID
 from google.cloud import storage
 from services.query.web_data_source import WebDataSource
 from utils.file_helper import process_upload_file, validate_multi_file_type
 from utils.gcs_helper import create_bucket_for_file
+
+Logger = Logger.get_logger(__file__)
 
 async def process_chat_file(chat_file,
                             chat_file_url,
@@ -39,8 +40,9 @@ async def process_chat_file(chat_file,
   
   Returns:
     list of URLs of chat files, Mime type of file
-  """  
+  """
   chat_file_type = None
+  chat_file_urls = None
   is_valid = False
   if chat_file is not None:
     bucket = create_bucket_for_file(chat_file.filename)
@@ -52,50 +54,53 @@ async def process_chat_file(chat_file,
       raise ValidationError(
           f"unsupported file type upload file {chat_file.filename}")
   elif chat_file_url:
-    if not (chat_file_url.startswith("gs://")
-            or chat_file_url.startswith("http://")
-            or chat_file_url.startswith("https://")
-            or chat_file_url.startswith("shpt://")):
-      return BadRequest(
+    parsed_url = urlparse(chat_file_url)
+
+    # validate url is supported protocol
+    if not parsed_url.scheme in ["gs", "http", "https", "shpt"]:
+      raise ValidationError(
           "chat_file_url must start with gs://, http:// or https://, shpt://")
-    # check file type from extension if present
-    chat_file_name = os.path.basename(chat_file_url)
-    file_extension = os.path.splitext(chat_file_name)[1]
-    if file_extension:
-      chat_file_type = validate_multi_file_type(chat_file_name)
-      if not is_valid:
-        raise ValidationError(
-            f"unsupported file type file url {chat_file_url}")
+
+    # validate file type from extension if present
+    chat_file_name = parsed_url.path
+    if chat_file_name:
+      file_extension = os.path.splitext(chat_file_name)[1]
+      if file_extension:
+        chat_file_type = validate_multi_file_type(chat_file_name)
+        if not is_valid:
+          raise ValidationError(
+              f"unsupported file type file url {chat_file_url}")
     else:
       # assume html if no extension
       chat_file_type = "text/html"
-  
+
     storage_client = storage.Client(project=PROJECT_ID)
-    if chat_file_url.startswith("http://") or \
-       chat_file_url.startswith("https://"):      
+    if parsed_url.scheme in ["http", "https"]:
       # download web site files
-      chat_file_name = chat_file_url.split("://")[1]
-      bucket = create_bucket_for_file(chat_file_name)
+      chat_file_path = chat_file_url.split("://")[1]
+      bucket = create_bucket_for_file(chat_file_path)
       web_data_source = WebDataSource(storage_client,
                                       bucket_name=bucket.name,
                                       depth_limit=depth_limit)
       with tempfile.TemporaryDirectory() as temp_dir:
-        data_source_files = web_data_source.download_documents(chat_file_url, temp_dir)
+        data_source_files = \
+            web_data_source.download_documents(chat_file_url, temp_dir)
         chat_file_urls = [f.gcs_url for f in data_source_files]
     elif chat_file_url.startswith("shpt://"):
       raise UnsupportedError("shpt:// not supported for chat upload")
     elif chat_file_url.startswith("gs://"):
       # process gcs url
-      gs_url = urlparse(chat_file_url)
-      bucket = storage_client.bucket(gs_url.netloc)
-      if not gs_url.path:
+      bucket = storage_client.bucket(parsed_url.netloc)
+      if not parsed_url.path:
         blobs = storage_client.list_blobs(
             bucket.name,
             delimiter="/",
             include_trailing_delimiter=True)
         chat_file_urls = [f"gs://{blob.name}" for blob in blobs]
       else:
-        chat_file_urls = [chat_file_url]        
+        chat_file_urls = [chat_file_url]
+
+  Logger.info(f"process_chat_upload: {chat_file_urls} type {chat_file_type}")
 
   return chat_file_urls, chat_file_type
 
