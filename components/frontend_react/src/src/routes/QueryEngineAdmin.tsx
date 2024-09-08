71214c95ebe935ebfc8936b4e730ecc9136a1065
Lukman Ramsey <lramsey@google.com>
2024-09-08 12:08:40 -0400

    comment out query file upload

--- components/frontend_react/src/src/routes/QueryEngineAdmin.tsx
+++ components/frontend_react/src/src/routes/QueryEngineAdmin.tsx
@@ -21,9 +21,16 @@ import QueryEngineEdit from "@/routes/QueryEngineEdit"
 import QueryEngineJobs from "@/routes/QueryEngineJobs"
 import QueryEngineSimple from "@/routes/QueryEngineSimple"
 
+// TODO: implement query file uploads
+//
+//enum TAB {
+//  QUERY_ENGINE_SIMPLE = "Query Engine Simple",
+//  QUERY_ENGINE = "Query Engine Advanced",
+//  QUERY_ENGINE_JOBS = "Query Engine Jobs",
+//}
+
 enum TAB {
-  QUERY_ENGINE_SIMPLE = "Query Engine Simple",
-  QUERY_ENGINE = "Query Engine Advanced",
+  QUERY_ENGINE = "Query Engine",
   QUERY_ENGINE_JOBS = "Query Engine Jobs",
 }
 
@@ -35,7 +42,7 @@ const QueryEngineAdmin: React.FC<IQueryEngineAdminProps> = ({ token }) => {
 
   const isAdmin = true // TODO: userStore((state) => state.isAdmin)
 
-  const [activeTab, setActiveTab] = useState<TAB>(TAB.QUERY_ENGINE_SIMPLE)
+  const [activeTab, setActiveTab] = useState<TAB>(TAB.QUERY_ENGINE)
 
   const params = useQueryParams()
   const id = params.get("qe_id")
@@ -63,18 +70,18 @@ const QueryEngineAdmin: React.FC<IQueryEngineAdminProps> = ({ token }) => {
   return (
     <div className="mb-4 mt-2 rounded-lg border border-base-300 bg-base-100 p-4 text-sm">
       <div className="tabs">
-        <div>{renderTab(TAB.QUERY_ENGINE_SIMPLE)}</div>
+        //<div>{renderTab(TAB.QUERY_ENGINE_SIMPLE)}</div>
         <div>{renderTab(TAB.QUERY_ENGINE)}</div>
         <div>{renderTab(TAB.QUERY_ENGINE_JOBS)}</div>
       </div>
-
       <div className="p-0">
+      {/*
         {activeTab === TAB.QUERY_ENGINE_SIMPLE && (
           <div className="border-t border-base-300 p-2">
             <QueryEngineSimple token={token!} />
           </div>
         )}
-
+      */}
         {activeTab === TAB.QUERY_ENGINE && (
           <div className="border-t border-base-300 p-2">
             <QueryEngineEdit token={token!} />
