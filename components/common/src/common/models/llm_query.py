cb930fb2838cc2730deb36d721f2cd849821d5e6
Shelley Cazares <shelleycazares@google.com>
2024-10-16 13:30:43 -0400

    Defined default values for optional fields of QueryReference and QueryDocumentChunk ORM objects, made chunk_id a required field

--- components/common/src/common/models/llm_query.py
+++ components/common/src/common/models/llm_query.py
@@ -201,13 +201,13 @@ class QueryReference(BaseModel):
   document_id = TextField(required=True)  # All modalities
   document_url = TextField(required=True)  # All modalities
   modality = TextField(required=True)  # All modalities: text image video audio
-  chunk_id = TextField(required=False)  # All modalities
-  chunk_url = TextField(required=False)  # Image or video or audio only
-  page = NumberField(required=False)  # Text or image only
-  document_text = TextField(required=False)  # Text only
-  timestamp_start = NumberField(required=False)  # Video or audio only
-  timestamp_stop = NumberField(required=False)  # Video or audio only
-  linked_ids = ListField(required=False)  # All modalities
+  chunk_id = TextField(required=True)  # All modalities
+  chunk_url = TextField(required=False, default=None)  # Image or video or audio only
+  page = NumberField(required=False, default=None)  # Text or image only
+  document_text = TextField(required=False, default=None)  # Text only
+  timestamp_start = NumberField(required=False, default=None)  # Video or audio only
+  timestamp_stop = NumberField(required=False, default=None)  # Video or audio only
+  linked_ids = ListField(IDField(), required=False, default=None)  # All modalities
 
   def __repr__(self) -> str:
     """
@@ -362,14 +362,14 @@ class QueryDocumentChunk(BaseModel):
   query_document_id = TextField(required=True)  # All modalities
   index = NumberField(required=True)  # All modalities
   modality = TextField(required=True)  # All modalities: text image video audio
-  page = NumberField(required=False)  # Text or image only
-  chunk_url = TextField(required=False)  # Image or video or audio only
-  text = TextField(required=False)  # Text only
-  clean_text = TextField(required=False)  # Text only (optional)
-  sentences = ListField(required=False)  # Text only (optional)
-  timestamp_start = NumberField(required=False)  # Video or audio only
-  timestamp_stop = NumberField(required=False)  # Video or audio only
-  linked_ids = ListField(required=False)  # All modalities
+  page = NumberField(required=False, default=None)  # Text or image only
+  chunk_url = TextField(required=False, default=None)  # Image or video or audio only
+  text = TextField(required=False, default=None)  # Text only
+  clean_text = TextField(required=False, default=None)  # Text only (optional)
+  sentences = ListField(required=False, default=None)  # Text only (optional)
+  timestamp_start = NumberField(required=False, default=None)  # Video or audio only
+  timestamp_stop = NumberField(required=False, default=None)  # Video or audio only
+  linked_ids = ListField(IDField(), required=False, default=None)  # All modalities
 
   class Meta:
     ignore_none_field = False
