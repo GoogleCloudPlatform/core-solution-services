a565e1464f5b92eba077cf0193f7cb0aa67056c3
Shelley Cazares <shelleycazares@google.com>
2024-10-23 21:18:15 -0400

    Linting fix

--- components/llm_service/src/services/query/data_source.py
+++ components/llm_service/src/services/query/data_source.py
@@ -289,8 +289,8 @@ class DataSource:
       if (doc_extension != "pdf" and
           doc_extension != "txt" and
           doc_extension not in allowed_image_types):
-          raise ValueError(f"{doc_name} must be a PDF, TXT, "
-                           f"PNG, JPG, BMP, or GIF")
+        raise ValueError(f"{doc_name} must be a PDF, TXT, "
+                         f"PNG, JPG, BMP, or GIF")
       # TODO: Insert elif statements to check for additional types of
       # videos (AVI, MP4, MOV, etc), and audio (MP3, WAV, etc)
     except Exception as e:
