29d4b6434017abbd9e8b1858ce13ffad81e8710e
smcazares <33378421+smcazares@users.noreply.github.com>
2024-10-23 20:33:36 -0400

    Update components/llm_service/src/services/query/data_source.py
    Co-authored-by: Robert Koehlmoos <RobertKoehlmoos@users.noreply.github.com>
    Signed-off-by: smcazares <33378421+smcazares@users.noreply.github.com>

--- components/llm_service/src/services/query/data_source.py
+++ components/llm_service/src/services/query/data_source.py
@@ -286,11 +286,11 @@ class DataSource:
     try:
       doc_extension = doc_name.split(".")[-1]
       doc_extension = doc_extension.lower()
-      if doc_extension != "pdf" and \
-        doc_extension != "txt" and \
-        doc_extension not in allowed_image_types:
-        raise ValueError(f"{doc_name} must be a PDF, TXT, "
-                         f"PNG, JPG, BMP, or GIF")
+      if (doc_extension != "pdf" and
+          doc_extension != "txt" and
+          doc_extension not in allowed_image_types):
+          raise ValueError(f"{doc_name} must be a PDF, TXT, "
+                           f"PNG, JPG, BMP, or GIF")
       # TODO: Insert elif statements to check for additional types of
       # videos (AVI, MP4, MOV, etc), and audio (MP3, WAV, etc)
     except Exception as e:
