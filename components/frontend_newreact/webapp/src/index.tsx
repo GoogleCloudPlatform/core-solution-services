518b4b077b7a5f4383c1dc5b4410aa8fdc9453aa
Lukman Ramsey <lramsey@google.com>
2025-02-06 16:50:15 -0500

    fix internationalization

--- components/frontend_newreact/webapp/src/index.tsx
+++ components/frontend_newreact/webapp/src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import './i18n' // Import the i18n configuration
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
