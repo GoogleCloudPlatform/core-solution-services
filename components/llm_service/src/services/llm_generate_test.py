45a389a8ba399cfa1da1ea690a600bdade8f8274
Lukman Ramsey <lramsey@google.com>
2025-02-08 09:46:09 -0500

    fix import

--- components/llm_service/src/services/llm_generate_test.py
+++ components/llm_service/src/services/llm_generate_test.py
@@ -28,8 +28,10 @@ os.environ["MODEL_GARDEN_LLAMA2_CHAT_ENDPOINT_ID"] = "fake-endpoint"
 os.environ["TRUSS_LLAMA2_ENDPOINT"] = "fake-endpoint"
 os.environ["VLLM_GEMMA_ENDPOINT"] = "fake-endpoint"
 
-from services.llm_generate import llm_generate, llm_chat, llm_generate_multimodal,\
-  llm_vllm_service_predict
+from services.llm_generate import (llm_generate, llm_chat, 
+                                 llm_generate_multimodal,
+                                 llm_vllm_service_predict, 
+                                 generate_chat_summary)
 from fastapi import UploadFile
 from google.cloud.aiplatform.models import Prediction
 from vertexai.preview.language_models import TextGenerationResponse
