8d5dbb8501cbe260971355c1dbe42d26f2996f43
Katherine Koehlmoos <ktko@google.com>
2025-03-14 17:19:40 -0700

    Continued set up. Need to create unique users

---
+++ tests/load_testing/example_config.py
@@ -0,0 +1,4 @@
+"""Configuration values to be used during load testing"""
+USERNAME = "example@example.com"
+PASSWORD = "password"
+BASE_URL = "https://mywebsite.com"
--- tests/load_testing/locustfile.py
+++ tests/load_testing/locustfile.py
@@ -14,23 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 import requests
+import uuid
 from locust import HttpUser, task
-
-
-USERNAME = "example@test.com"
-PASSWORD = "password"
-BASE_URL = "https://my-genie.website.com/"
-
-class GetChatTypesUser(HttpUser):
-  """Performs a basic get request to verify load testing"""
-  def on_start(self):
-    id_token = get_token(USERNAME, PASSWORD, BASE_URL)
-    self.headers = {"Authorization": f"Bearer {id_token}"}
-
-  @task
-  def get_chat_types(self):
-    chat_types_url = f"{BASE_URL}/llm-service/api/v1/chat/chat_types"
-    self.client.get(chat_types_url, headers=self.headers, timeout=10)
+from config import USERNAME, PASSWORD, BASE_URL
 
 def get_token(user_email: str, user_password: str, base_url: str) -> str:
   req_body = {
@@ -42,5 +28,36 @@ def get_token(user_email: str, user_password: str, base_url: str) -> str:
                               timeout=10)
   sign_in_res = sign_in_req.json()
   id_token = sign_in_res["data"]["idToken"]
-  print(id_token)
   return id_token
+
+# getting an initail token to be shared between all users
+# all requests are from a single user, future work can add support for requests
+# coming from unique users
+INITIAL_ID_TOKEN = get_token(USERNAME, PASSWORD, BASE_URL)
+
+class GetChatTypesUser(HttpUser):
+  """Performs a basic get request to verify load testing"""
+  def on_start(self):
+    self.headers = {"Authorization": f"Bearer {INITIAL_ID_TOKEN}"}
+
+  @task
+  def get_chat_types(self):
+    chat_types_url = f"{BASE_URL}/llm-service/api/v1/chat/chat_types"
+    self.client.get(chat_types_url, headers=self.headers, timeout=10)
+
+  @task
+  def create_chat_and_ask_question(self):
+    create_chat_url = f"{BASE_URL}/llm-service/api/v1/chat/empty_chat"
+    create_chat_resp = self.client.post(
+      create_chat_url, timeout=10,
+      headers=self.headers)
+    chat_id = create_chat_resp.json()["data"]["id"]
+    continue_chat_url = f"{BASE_URL}/llm-service/api/v1/chat/{chat_id}/generate"
+    # using a common name allows locust to group together api calls with
+    # path parameters
+    common_name = f"{BASE_URL}/llm-service/api/v1/chat/<chat_id>/generate"
+    req_body = {"prompt": "Name the continents", "llm_type": "VertexAI-Chat"}
+    self.client.post(continue_chat_url, headers=self.headers,
+                     timeout=10, json=req_body, name=common_name)
+
+
