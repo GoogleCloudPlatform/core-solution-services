1d8af466d7f7c896298058096a69701bdf903ff1
Katherine Koehlmoos <ktko@google.com>
2025-04-08 15:08:59 -0700

    Test fix

--- components/llm_service/src/routes/chat_test.py
+++ components/llm_service/src/routes/chat_test.py
@@ -139,7 +139,7 @@ def test_get_chat(create_user, create_chat, client_with_emulator):
 @pytest.mark.asyncio
 async def test_create_chat(create_user, client_with_emulator):
   """Test creating a new chat"""
-  url = f"{api_url}\empty_chat"
+  url = f"{api_url}/empty_chat"
   resp = client_with_emulator.post(url)
   assert resp.status_code == 200, "Failed to create empty chat"
   json_response = resp.json()
